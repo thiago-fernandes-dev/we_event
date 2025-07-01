07c39aa15522d153b690140935629ef9ff992746
thiago-fernandes-dev <thiagod_paulo@hotmail.com>
2025-07-01 15:57:31 -0300

    preparando os próximos componentes

--- src/components/ContainerBanner.jsx
+++ src/components/ContainerBanner.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export function ContainerBanner(){
     return (
-        <section className=" w-full h-1/3">
-            <img id="banner1" className=" object-cover h-1/3" src="src/assets/shows.jpg" alt="evento-banner"/>
+        <section className="flex w-full h-100">
+            <img id="banner1" className="flex object-cover object-[25%_75%] w-full h-full max-h-1/2" src="src/assets/shows.jpg" alt="evento-banner"/>
             <a href="#"></a>
         </section>
     )
