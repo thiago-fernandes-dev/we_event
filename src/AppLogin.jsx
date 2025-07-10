00c9018cedcc414b81518548d20c5365c87a8995
ThiagoSampaio88 <thiago88sampaio.dev@gmail.com>
2025-07-10 15:45:43 -0300

    Componente contendo a chamada do login e cadastro

---
+++ src/AppLogin.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import Cadastro from "./components/Cadastro";
+import Login from "./components/Login";
+
+
+export default function AppLogin() {
+  return(
+    <Fragment>
+        <Cadastro/>    
+      {/* <Login/>*/}
+    </Fragment>
+  )
+}
