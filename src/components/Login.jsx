a039a77501618ca594e83aa471f68dffeb18b6a9
thiago-fernandes-dev <thiagod_paulo@hotmail.com>
2025-07-16 03:21:04 -0300

    criando a branch pra juntar tudo e deixar pronto pra jogar na main

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { useState } from "react";
 import {useNavigate} from "react-router-dom";
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import React from "react";
-import {BrowserRouter, Route, Routes, Link } from "react-router-dom";
-import {RouterPage} from "./components/RouterPage.jsx";
-
+import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
